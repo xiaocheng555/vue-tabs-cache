6c22fd72878aa94f338361b5a04e9071fb445697
xiaocheng555 <1052745517@qq.com>
2022-10-22 23:59:43 +0800

    feat: 优化

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app">
     <router-view></router-view>
   </div>
 </template>
@@ -16,5 +16,13 @@ collectCaches()
 * {
   margin: 0;
   padding: 0;
+}
+html, body {
+  overflow-y: hidden;
+}
+@media (max-width: 768px) {
+  #app {
+    width: 200%;
+  }
 }
 </style>
